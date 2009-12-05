aef3098e3fc94a4f650e669c352b36cc89a8c611
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2009-12-04 20:37:44 -0500

    rewrote using asyncore/asynchat

--- quorum/quorumd.py
+++ quorum/quorumd.py
@@ -1,129 +1,225 @@
+import sys
 import logging
+import asyncore
+import asynchat
+import socket
 import random
 import string
-import time
 
-import cherrypy
-from cherrypy import _cptools
+STATE_INITIAL   = 0
+STATE_AUTH      = 1
+STATE_NEWREQ    = 2
+STATE_GETREQ    = 3
+STATE_VOTED     = 4
 
-class AuthenticationFailed (Exception):
+class QuorumException(Exception):
+    pass
+
+class AuthenticationFailed(QuorumException):
     def __init__(self):
-        super(AuthenticationFailed, self).__init__('Authentication failed.')
-
-class ActiveRequest (Exception):
+        super(AuthenticationFailed, self).__init__(
+                'Authentication failed.')
+class PendingRequest(QuorumException):
     def __init__(self):
-        super(ActiveRequest, self).__init__('There is already an active request.')
+        super(PendingRequest, self).__init__(
+                'There is already a pending request.')
+class InvalidRequest(QuorumException):
+    def __init__(self):
+        super(InvalidRequest, self).__init__(
+                'Invalid request.')
+class NotImplemented(QuorumException):
+    def __init__(self):
+        super(NotImplemented, self).__init__(
+                'That command is not implemented.')
+class DuplicateVote(QuorumException):
+    def __init__(self):
+        super(DuplicateVote, self).__init__(
+                'You have already voted.')
+class NoVoteForYou(QuorumException):
+    def __init__(self):
+        super(NoVoteForYou, self).__init__(
+                'You may not vote on your own request.')
+class InvalidCommand(QuorumException):
+    def __init__(self):
+        super(InvalidCommand, self).__init__(
+                'That command is not valid at this time.')
 
-class InvalidRequest (Exception):
-    def __init__(self, rid):
-        super(InvalidRequest, self).__init__('Invalid request (%s).' % rid)
-
-def authenticated (f):
-    def _(self, tok, *args, **kwargs):
-        if not self.check_tok(tok):
+def authenticated(f):
+    def _(self, *args, **kwargs):
+        if not self.authenticated:
+            self.server.log.error('%s called by unauthenticated client.' \
+                    % f.__name__)
             raise AuthenticationFailed()
 
         return f(self, *args, **kwargs)
 
-    _.__name__ == f.__name__
     return _
 
-class Quorum(_cptools.XMLRPCController):
-    def __init__(self):
-        self._sessions = {}
-        self._auth_validity = 300
-        self._request = None
-        self._required_votes = 3
-        self._unanimous = False
+class QuorumChannel(asynchat.async_chat):
+    def __init__(self, server, sock, addr):
+        asynchat.async_chat.__init__(self, sock)
+        self.server = server
+        self.set_terminator("\r\n")
+        self.data = []
+        self.authenticated = False
 
-    @cherrypy.expose
-    def login(self, name, password):
-        tok = ''.join(random.sample(string.letters + string.digits, 16))
-        self._sessions[tok] = { 'when': time.time(), 'name': name }
-        return tok
-
-    @cherrypy.expose
-    @authenticated
-    def request(self, command):
-        print self._sessions
-
-        if self._request:
-            raise ActiveRequest()
-
-        rid = ''.join(random.sample(string.letters + string.digits, 16))
-        self._request = {
-                'rid': rid,
-                'votes': 0, 
-                'voters': {},
-                'requestor': cherrypy.request.session['name'],
-                'command': command,
+        self.state = 0
+        self.states = {
+                STATE_INITIAL   : ('AUTH', 'QUIT'),
+                STATE_AUTH      : ('QUIT', 'NEWREQ', 'GETREQ'),
+                STATE_GETREQ    : ('QUIT', 'VOTE'),
+                STATE_NEWREQ    : ('QUIT'),
+                STATE_VOTED     : ('QUIT'),
                 }
 
-        return rid
+    def collect_incoming_data(self, data):
+        self.data.append(data)
 
-    @cherrypy.expose
-    @authenticated
-    def check(self):
-        pass
+    def found_terminator(self):
+        try:
+            command, args = ''.join(self.data).split(None, 1)
+            args = args.split()
+        except:
+            command = ''.join(self.data)
+            args = []
+        command = command.upper()
+        self.data = []
 
-    @cherrypy.expose
-    @authenticated
-    def respond(self, rid, response):
-        if not self._request['rid'] == rid:
-            raise InvalidRequest(rid)
+        try:
+            self.server.log.info('Command: %s (%s).' \
+                    % (command, ','.join(args)))
 
-        if response:
-            if not cherrypy.request.session['name'] in self._request['voters']:
-                self._request['votes'] += 1
-                self._request['voters'][cherrypy.request.session['name']] = response
+            if command in self.states[self.state]:
+                if hasattr(self, 'handle_%s' % command):
+                    getattr(self, 'handle_%s' % command)(*args)
+                else:
+                    raise NotImplemented()
             else:
-                pass
-        elif self._unanimous:
-            pass
+                raise InvalidCommand()
+        except QuorumException, detail:
+            self.err(str(detail))
+        except Exception, detail:
+            self.err(str(detail))
+            self.close_when_done()
+            self.shutdown = True
+
+    def okay(self, msg='Okay.'):
+        self.push('OK %s\r\n' % msg)
+
+    def err(self, msg='Error.'):
+        self.push('ERR %s\r\n' % msg)
+
+    def handle_AUTH(self, name, password):
+        if self.server.authenticate(name, password):
+            self.authenticated = True
+            self.name = name.lower()
+            self.state = STATE_AUTH
+            self.okay('Authenticated.')
         else:
-            pass
+            raise AuthenticationFailed()
 
-        return True
-
-    @cherrypy.expose
-    def dump(self, what='sessions'):
-        if what == 'sessions':
-            return self._sessions
-        elif what == 'request':
-            return self._request
-        else:
-            return True
-
-    @cherrypy.expose
     @authenticated
-    def poll(self, rid):
-        pass
+    def handle_NEWREQ(self, label):
+        self.server.newreq(self, label)
+        self.state = STATE_NEWREQ
+        self.okay('Your request has been posted.')
 
-    def run(self):
-        cherrypy.quickstart(self, config={
-            '/': {'request.dispatch': cherrypy.dispatch.XMLRPCDispatcher()},
-            })
+    @authenticated
+    def handle_GETREQ(self):
+        req = self.server.getreq()
+        print 'REQ:', req
+        if req:
+            self.state = STATE_GETREQ
+            self.push('* %(id)s %(requestor)s %(label)s\r\n' % \
+                    self.server.request)
+        self.okay()
 
-    def check_tok(self, tok):
-        sess = self._sessions.get(tok)
+    @authenticated
+    def handle_VOTE(self, reqid, response):
+        self.server.register_response(reqid, self, response)
+        self.okay('Your vote has been received.')
+        self.state = STATE_VOTED
 
-        if not sess:
-            return False
+    def handle_QUIT(self):
+        self.okay('Quitting.')
+        self.close_when_done()
+
+class QuorumServer(asyncore.dispatcher):
+
+    def __init__(self):
+        asyncore.dispatcher.__init__(self)
         
-        if sess['when'] < time.time() - self._auth_validity:
-            del self._sessions[tok]
-            return False
+        self.log = logging.getLogger('QuorumServer')
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.bind(('', 9000))
+        self.listen(5)
 
-        cherrypy.request.session = sess
+        self.request = None
 
+    def handle_accept(self):
+        conn, addr = self.accept()
+        self.log.info('Accepted connection from %s.' % str(addr))
+        QuorumChannel(self, conn, addr)
+
+    def loop(self):
+        self.log.info('Starting loop.')
+        asyncore.loop()
+
+    def authenticate(self, user, password):
         return True
 
-def main():
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.INFO)
-    quorum = Quorum()
-    quorum.run()
+    def getreq(self):
+        return self.request
+
+    def newreq(self, requestor, label):
+        if self.request:
+            raise PendingRequest()
+
+        self.request = {
+                'id': self.get_request_id(),
+                'requestor': requestor.name,
+                'label': label,
+                'required_votes': 3,
+                'votes': {},
+                'unanimous': False,
+                }
+        self.log.info('New request: %(label)s from %(requestor)s.' \
+                % self.request)
+
+    def get_request_id(self):
+        return ''.join(random.sample(\
+                string.letters + string.digits, 16))
+
+    def register_response(self, reqid, responder, response):
+        if not self.request or not reqid == self.request['id']:
+            raise InvalidRequest()
+
+        if responder.name == self.request['requestor']:
+            raise NoVoteForYou()
+        if responder.name in self.request['votes']:
+            raise DuplicateVote()
+
+        self.request['votes'][responder.name] = (
+                int(response), responder)
+
+        self.tally()
+
+    def tally(self):
+        yesvotes = 0
+        novotes = 0
+        for k, v in self.request['votes'].items():
+            if v[0]:
+                yesvotes += 1
+            else:
+                novotes += 1
+        
+        self.log.info('Request %s: %d for, %d against.' \
+                % (self.request['id'], yesvotes, novotes))
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    s = QuorumServer()
+    s.loop()
 
