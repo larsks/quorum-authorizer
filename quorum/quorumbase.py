1fbfc50571e0be43c4d11d6f10daa25dd9e8ce12
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2009-12-17 21:32:49 -0500

    whoops, forgot to add quorumbase in last push

---
+++ quorum/quorumbase.py
@@ -0,0 +1,26 @@
+import os
+import logging
+
+from qexceptions import *
+
+class QuorumBase (object):
+
+    def __init__ (self, config, logger='quorum'):
+        self.config = config
+        self.log = logging.getLogger(logger)
+        self.quorum_dir = os.path.join(
+                self.config.get('quorum', 'quorum directory parent'),
+                self.config.get('quorum', 'quorum directory'))
+
+        if not os.path.isdir(self.quorum_dir):
+            raise ConfigurationError(
+                    'Quorum directory %s does not exist.' % self.quorum_dir)
+
+    def validate(self, req):
+        if not self.config.has_section('command %s' % req):
+            raise ConfigurationError(
+                    'Request %s does not match a configured command.' % req)
+
+
+        return os.path.join(self.quorum_dir, req)
+
